066419eeccf427d0dcf20c6087a48b03ea5c1399
Moahemd-basawy <mohamedbadawysayed02@gmail.com>
2024-06-09 12:07:18 +0300

    update

--- js/Script.js
+++ js/Script.js
@@ -2,3 +2,6 @@
  var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
 // auto scroll logo slider
+
+var scrollPartners = document.getElementById("Logos");
+
